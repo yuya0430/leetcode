038c2843318ca10d627f0b2072055f0332545f39
yuya0430 <yuya0430@slp.nitech.ac.jp>
2021-01-24 22:30:44 +0900

    twosum challenge

---
+++ Twosum.py
@@ -0,0 +1,72 @@
+import numpy as np
+import re
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :type output: List[int]
+        """
+        length = len(nums)
+        array = []
+        for i in range(length):
+            array.append(nums)
+
+        array = np.array(array)
+        t_array = array.transpose()
+        
+        sum_result = array + t_array
+        
+        answer = ''
+        for i in range(length - 1):
+            for j in np.arange(i+1, length):
+                if sum_result[i][j] == target:
+                    answer = "[" + str(i) + "," + str(j) + "]"
+
+        return answer
+
+def main():
+    solution = Solution()
+    while True:
+        nums_str = input()
+        
+        nums_list = re.split('[\[,\]]', nums_str)
+
+        nums = []
+        for num in nums_list:
+            if num != '':
+                try:
+                    if int(num) >= -10**9 and int(num) <= 10**9:
+                        nums.append(int(num))
+                    else:
+                        print("num is \"-10^9 <= num <= 10^9\"")
+                        continue
+                except ValueError as e:
+                    print("input example: [1,2,4,7]")
+                    continue
+        
+        length = len(nums)
+        if length >= 2 and length <= 1000:
+            break
+        else:
+            print("length of list is \"2 <= num <= 1000\"")
+
+    while True:
+        try:
+            target = int(input())
+        except ValueError as e:
+            print("int only")
+            continue
+
+        if target >= -10**9 and target <= 10**9:
+            break
+        else:
+            print("target is \"-10^9 <= target <= 10^9\"")
+    
+    answer = solution.twoSum(nums, target)
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
